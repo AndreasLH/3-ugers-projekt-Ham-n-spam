909136d72cd3ae97614390ceee18595462c3789c
Andreas Lau Hansen <s194235@student.dtu.dk>
2020-01-10 09:33:37 +0100

    SVM implemented

--- protoype.py
+++ protoype.py
@@ -4,36 +4,30 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('processed_emails.csv')
-#url = 'https://github.com/AndreasLH/3-ugers-projekt-Ham-n-spam/blob/master/emails.csv'
+#df = pd.read_csv('processed_emails.csv')
+unprocc = pd.read_csv('emails.csv')
+
+#url = 'https://raw.githubusercontent.com/AndreasLH/3-ugers-projekt-Ham-n-spam/master/processed_emails.csv'
 #df = pd.read_csv(url)
+#alternatively
+df = pd.read_csv('processed_email.csv')
+
 x = df.text
 y = df.spam
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(x, y, random_state = 1)
+X_train, X_test, y_train, y_test = train_test_split(x, y, random_state = 1, test_size = 0.3)
 print('shape of train and test matrix')
 print(X_train.shape)
 print(X_test.shape)
 print(y_train.shape)
 print(y_test.shape)
 
-# =============================================================================
-# from sklearn.feature_extraction.text import CountVectorizer
-# #dette er bag of words!
-# vect = CountVectorizer()
-# vect.fit(X_train)
-# X_train_dtm = vect.transform(X_train)
-# #tilsvarende til de 2 forrige linjer (fit og transform)
-# #X_train_dtm = vect.fit_transform(X_train)
-# print(X_train_dtm.shape)
-# X_test_dtm = vect.transform(X_test)
-# print(X_test_dtm.shape)
-# =============================================================================
+
 
 #TF-IDF implementation
 from sklearn.feature_extraction.text import TfidfVectorizer
-#dette er bag of words!
+#dette er TF-IDF
 vect = TfidfVectorizer()
 X_train_dtm = vect.fit_transform(X_train)
 print('feature matrix TD-IDF sparse matrix')
@@ -42,10 +36,14 @@ X_test_dtm = vect.transform(X_test)
 #print(X_test_dtm.shape)
 
 #make model
-from sklearn.naive_bayes import MultinomialNB
-nb = MultinomialNB()
-nb.fit(X_train_dtm, y_train)
-y_pred_class = nb.predict(X_test_dtm)
+#from sklearn.naive_bayes import MultinomialNB
+#nb = MultinomialNB()
+#nb.fit(X_train_dtm, y_train)
+#y_pred_class = nb.predict(X_test_dtm)
+from sklearn import svm
+clf =  svm.SVC()
+clf.fit(X_train_dtm, y_train)
+y_pred_class = clf.predict(X_test_dtm)
 
 from sklearn import metrics
 #accuracy
@@ -70,14 +68,29 @@ print('false negative messages i.e. spam emails wrongly classified as ham emails
 print(X_test[y_pred_class < y_test])
 
 #naive bayes er ikke så præcis når den siger at det med 100% er spam, det er bare udregnet
-y_pred_prob = nb.predict_proba(X_test_dtm)[:, 1]
-print(y_pred_prob)
+#y_pred_prob = nb.predict_proba(X_test_dtm)[:, 1]
+#print(y_pred_prob)
 #area under curve
-AUC = metrics.roc_auc_score(y_test, y_pred_prob)
-print('area under curve', AUC)
+#AUC = metrics.roc_auc_score(y_test, y_pred_prob)
+#print('area under curve', AUC)
 
 
 """
+implementation with bag of words
+# =============================================================================
+# from sklearn.feature_extraction.text import CountVectorizer
+# #dette er bag of words!
+# vect = CountVectorizer()
+# vect.fit(X_train)
+# X_train_dtm = vect.transform(X_train)
+# #tilsvarende til de 2 forrige linjer (fit og transform)
+# #X_train_dtm = vect.fit_transform(X_train)
+# print(X_train_dtm.shape)
+# X_test_dtm = vect.transform(X_test)
+# print(X_test_dtm.shape)
+# =============================================================================
+
+
 arr = np.array(df)
 
 simple_train = arr[0, 0]
